bef4edc4f4d4bbe22b45cdaf12c2431d52f55bd7
Chris <cj@squiggs.com>
2017-07-03 14:46:30 -0400

    adding headr elements

--- gulp/task/styles.js
+++ gulp/task/styles.js
@@ -4,12 +4,13 @@ var gulp = require('gulp'),
 	cssvars = require('postcss-simple-vars'),
 	nested = require('postcss-nested'),
 	cssImport = require('postcss-import'),
-	mixins = require('postcss-mixins');
+	mixins = require('postcss-mixins'),
+	hexrgba = require('postcss-hexrgba');
 
 gulp.task('styles', function(){
 	console.log('START CSS or SASS');
 	return gulp.src('./app/assets/css/styles.css')
-		.pipe(postcss([cssImport, mixins, cssvars, nested, autoprefixer]))
+		.pipe(postcss([cssImport, mixins, cssvars, nested, hexrgba, autoprefixer]))
 		.on('error', function(errorInfo){
 			console.log(errorInfo.toString());
 			this.emit('end');
